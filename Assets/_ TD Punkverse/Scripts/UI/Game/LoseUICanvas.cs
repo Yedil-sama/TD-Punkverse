2af4e4544f92704a40d7d2425fc88c82dc77e2bd
Abdullaev Fakhridin <102026234+Faha505@users.noreply.github.com>
2025-11-30 09:45:01 +0500

    Polishing Buttons

--- Assets/_ TD Punkverse/Scripts/UI/Game/LoseUICanvas.cs
+++ Assets/_ TD Punkverse/Scripts/UI/Game/LoseUICanvas.cs
@@ -4,9 +4,12 @@ namespace TD_Punkverse.UI.Game
 {
 	public sealed class LoseUICanvas : UICanvas
 	{
+		private SceneLoaderService _sceneLoaderService;
 		protected override void Initialize()
 		{
 			base.Initialize();
+
+			_sceneLoaderService = ServiceLocator.Instance.Get<SceneLoaderService>();
 
 			Subscribe();
 		}
@@ -18,5 +21,15 @@ namespace TD_Punkverse.UI.Game
 
 		private void Subscribe() => GameEventBus.OnLose += Open;
 		private void Unsubscribe() => GameEventBus.OnLose -= Open;
-	}
+
+        public void OnRetryButtonPress()
+        {
+            _sceneLoaderService.LoadScene("Game Scene");
+        }
+
+        public void OnMenuButtonPress()
+        {
+            _sceneLoaderService.LoadScene("Menu Scene");
+        }
+    }
 }
--- Assets/_ TD Punkverse/Scripts/UI/Game/WinUICanvas.cs
+++ Assets/_ TD Punkverse/Scripts/UI/Game/WinUICanvas.cs
@@ -4,9 +4,13 @@ namespace TD_Punkverse.UI.Game
 {
 	public sealed class WinUICanvas : UICanvas
 	{
+		private SceneLoaderService _sceneLoaderService;
+
 		protected override void Initialize()
 		{
 			base.Initialize();
+
+			_sceneLoaderService = ServiceLocator.Instance.Get<SceneLoaderService>();
 
 			Subscribe();
 		}
@@ -18,5 +22,15 @@ namespace TD_Punkverse.UI.Game
 
 		private void Subscribe() => GameEventBus.OnWin += Open;
 		private void Unsubscribe() => GameEventBus.OnWin -= Open;
+
+		public void OnRetryButtonPress()
+		{
+            _sceneLoaderService.LoadScene("Game Scene");
+        }
+
+		public void OnMenuButtonPress()
+		{
+            _sceneLoaderService.LoadScene("Menu Scene");
+        }
 	}
 }
